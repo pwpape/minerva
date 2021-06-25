ac71811f1de64fb5802cda5cbc2e049ce4631485
Patrick Pape <asiOtus@localhost.localdomain>
2021-06-25 12:11:16 -0500

    laravel initialization

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+
+<title>Minerva's Vault</title>
+<link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css" >
+
+<body>
+    <div class="budget">
+        <div>
+            <span>Housing</span>
+            <input type="text">
+        </div>
+        <div>
+            <span>Life, DI, and LTC</span>
+            <input type="text">
+        </div>
+        <div>
+            <span>Property and Casualty</span>
+            <input type="text">
+        </div>
+        <div>
+            <span>Electricity</span>
+            <input type="text">
+        </div>
+        <div>
+            <span>Gas</span>
+            <input type="text">
+        </div>
+        <div>
+            <span>Water</span>
+            <input type="text">
+        </div>
+        <div>
+            <span>Streaming</span>
+            <input type="text">
+        </div>
+        <div>
+            <span>Golf</span>
+            <input type="text">
+        </div>
+    </div>
+</body>
